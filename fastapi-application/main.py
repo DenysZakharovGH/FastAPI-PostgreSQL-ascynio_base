0da7b7704d2127a1e93f60d177816b26bdbf9604
Denys Zakharov <deniszaharov19981208@gmail.com>
2025-01-30 18:26:49 +0100

    Created main settings

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,11 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from api import router as api_router
+from core.config import settings
 
-app = FastAPI
 
+app = FastAPI()
+app.include_router(api_router,
+                   prefix=settings.api.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app",
+                host=settings.run.host,
+                port=settings.run.port,
+                reload=True)
     # Will watch for changes in these directories
     # create automatic reload with saving files,
     # show path to file and fast API object to reload
