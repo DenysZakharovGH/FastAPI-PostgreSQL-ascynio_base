443340ae6ffe613e911e420f67fd80493e7b19f2
Denys Zakharov <deniszaharov19981208@gmail.com>
2025-01-31 00:33:10 +0100

    Add naming convertion for Models

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,8 +1,14 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy.orm import Mapped, mapped_column
 
+from core.config import settings
+
+
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     # trick to create a table name in DB with same name from small letter as name of class
     @declared_attr.directive
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,6 +1,15 @@
+from sqlalchemy import UniqueConstraint
+
 from core.models.base import Base
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
+    age: Mapped[int]
+    email: Mapped[str]
+
+    # cant be second user with same email
+    __table_args__ = (
+        UniqueConstraint("email"),
+    )
